575c170dc24cc9117031426addcd1c1c33454a5c
dominikg <dominik.goepel@gmx.de>
2022-12-14 11:07:28 +0100

    fix: add code to strip -next tag from peer and dev deps

--- .changeset/exit-pre-manually.js
+++ .changeset/exit-pre-manually.js
@@ -20,6 +20,15 @@ for (const pkg_file of package_files) {
 	const { version } = pkg;
 	if (version.includes('-next')) {
 		pkg.version = version.replace(/(\d+\.\d+\.\d+)-next.*/, '$1');
+		// remove -next from peerDependencies
+		if (pkg.peerDependencies) {
+			pkg.peerDependencies = Object.fromEntries(
+				Object.entries(pkg.peerDependencies).map(([k, v]) => [
+					k,
+					v.replace(/(\d+\.\d+\.\d+)-next.*/, '$1')
+				])
+			);
+		}
 		writeFileSync(pkg_file, JSON.stringify(pkg, null, '\t') + '\n');
 		const changelog_file = pkg_file.replace(/package\.json$/, 'CHANGELOG.md');
 		let changelog = readFileSync(changelog_file, 'utf-8');
@@ -29,6 +38,24 @@ for (const pkg_file of package_files) {
 		throw new Error(`invalid version ${version}, expected -next tag for ${pkg.name}`);
 	}
 	console.log(`updated ${pkg.name}@${version} to ${pkg.version}`);
+}
+const create_svelte_template_dir = fileURLToPath(
+	new URL('../packages/create-svelte/templates', import.meta.url)
+);
+const create_svelte_templates = glob(`${create_svelte_template_dir}/*/package.template.json`);
+for (const create_svelte_template_pkg_file of create_svelte_templates) {
+	const pkg = JSON.parse(readFileSync(create_svelte_template_pkg_file, 'utf-8'));
+	// remove -next from devDependencies
+	if (pkg.devDependencies) {
+		pkg.devDependencies = Object.fromEntries(
+			Object.entries(pkg.devDependencies).map(([k, v]) => [
+				k,
+				v.replace(/(\d+\.\d+\.\d+)-next.*/, '$1')
+			])
+		);
+	}
+	writeFileSync(create_svelte_template_pkg_file, JSON.stringify(pkg, null, '\t') + '\n');
+	console.log(`removed -next from devDependencies in ${create_svelte_template_pkg_file}`);
 }
 
 function update_changelog(changelog, pkg) {
